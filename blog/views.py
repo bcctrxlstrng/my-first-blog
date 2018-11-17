e0ea626356ffcc448067882154efbc2f0d65e86c
becca <bcctrxlstrng@gmail.com>
2018-11-17 13:42:39 +1100

    Changed home page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post # the . is for current directory or current application
+
+# to take actual blog posts from the Post model, we use QuerySet
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
